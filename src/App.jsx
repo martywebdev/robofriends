33f9a4970de8cd6d190db87d2f75c0961257326f
marty <martyumlas@gmail.com>
2024-08-04 10:13:00 +0800

    add tachyons

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div>Hello World</div>
+    <div className='f1 tc'>
+      <h1>Hello World</h1>
+      <p>Welcome to React</p>
+    </div>
   )
 }
 
